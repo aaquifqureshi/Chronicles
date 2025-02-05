30bd5b3f05f021c53d1c2d8a32d365ba66387a0c
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2025-02-06 00:49:43 +0530

    2 Alert Components Implemented
    2 Alert Components using rFlutter_alert, we created noInternetAlert and authFailedAlert.

---
+++ lib/utilities/components/alerts/auth_failed_alert.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+AlertStyle authFailedAlertStyle = AlertStyle(
+  backgroundColor: Color(0xFFFFFFFF),
+  descStyle: TextStyle(
+    color: Color(0xFF1F1F1F),
+  ),
+  alertBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(6.0),
+    side: BorderSide(
+      color: Color(0xFF111519),
+    ),
+  ),
+  titleStyle: TextStyle(
+    color: Color(0xFF4EABCC),
+    fontWeight: FontWeight.bold,
+    fontSize: 30.0,
+  ),
+);
+
+TextStyle authFailedAlertTextStyle =
+    TextStyle(color: Color(0xFFFFFFFF), fontSize: 20);
+
+void authFailedAlert(BuildContext context) {
+  Alert(
+    context: context,
+    title: "Access Denied",
+    desc: "Please check username and/or password...",
+    style: authFailedAlertStyle,
+    buttons: [
+      DialogButton(
+        onPressed: () => Navigator.pop(context),
+        color: Color(0xFF4EABCC),
+        child: Text(
+          "Try Again!",
+          style: authFailedAlertTextStyle,
+        ),
+      ),
+    ],
+  ).show();
+}
---
+++ lib/utilities/components/alerts/no_internet_alert.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+AlertStyle noInternetAlertStyle = AlertStyle(
+  backgroundColor: Color(0xFFFFFFFF),
+  descStyle: TextStyle(
+    color: Color(0xFF1F1F1F),
+  ),
+  alertBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(6.0),
+    side: BorderSide(
+      color: Color(0xFF111519),
+    ),
+  ),
+  titleStyle: TextStyle(
+    color: Color(0xFF4EABCC),
+    fontWeight: FontWeight.bold,
+    fontSize: 30.0,
+  ),
+);
+
+TextStyle noInternetAlertTextStyle =
+    TextStyle(color: Color(0xFFFFFFFF), fontSize: 20);
+
+void noInternetAlert(BuildContext context) {
+  Alert(
+    context: context,
+    title: "No Internet Access",
+    desc: "Please connect to the internet, this is one time process...",
+    style: noInternetAlertStyle,
+    buttons: [
+      DialogButton(
+        onPressed: () => Navigator.pop(context),
+        color: Color(0xFF4EABCC),
+        child: Text(
+          "Sorry!",
+          style: noInternetAlertTextStyle,
+        ),
+      ),
+    ],
+  ).show();
+}

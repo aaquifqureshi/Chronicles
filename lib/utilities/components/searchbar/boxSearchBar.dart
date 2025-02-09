a3d6aa19b5653ed7d73c7efecdb4ad1776e3d715
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2025-02-09 22:50:48 +0530

    Searchbar Fix
    Searchbar Fix

--- lib/utilities/components/searchbar/boxSearchBar.dart
+++ lib/utilities/components/searchbar/boxSearchBar.dart
@@ -10,6 +10,24 @@ class BoxSearchBar extends StatefulWidget {
 class _BoxSearchBarState extends State<BoxSearchBar> {
   @override
   Widget build(BuildContext context) {
-    return SearchBar();
+    return SearchBar(
+      hintText: "Search",
+      hintStyle: WidgetStateProperty.all<TextStyle>(
+        TextStyle(
+          fontSize: 16.0,
+          fontFamily: 'Hind',
+          fontWeight: FontWeight.w400,
+          color: Color(0xFF1F1F1F),
+        ),
+      ),
+      leading: Icon(Icons.search, color: Color(0xFF1F1F1F)),
+      backgroundColor: WidgetStateProperty.all<Color>(Colors.white),
+      shadowColor: WidgetStateProperty.all<Color>(Color(0xFF344A53)),
+      elevation: WidgetStateProperty.all<double>(2),
+      padding:
+          WidgetStateProperty.all<EdgeInsets>(EdgeInsets.fromLTRB(22, 0, 0, 0)),
+      shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(3))),
+    );
   }
 }

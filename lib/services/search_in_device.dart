f9c1a0cf0825d1cb125b64e072b321ec4f10ef69
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2025-03-21 16:30:08 +0530

    Search Bar Implemented
    Search Bar Implemented, Bugs exist...
    
    SearchTextField not clearing.

--- lib/services/search_in_device.dart
+++ lib/services/search_in_device.dart
@@ -4,3 +4,39 @@
 * Description      : This file is has code for Searching File Database,
 *                    File Information, and all data stored.
 */
+
+import 'package:chronicles/services/todo_services.dart';
+import '../utilities/components/text_editor/file_data_class.dart';
+import '../utilities/components/todo/todo.dart';
+import 'file_database.dart';
+
+class SearchService {
+  final ToDoDatabaseService _todoService = ToDoDatabaseService.instance;
+  final FileDatabase _fileService = FileDatabase.instance;
+
+  Future<List<ToDo>> getTodoSuggestions(String input) async {
+    List<ToDo> todoSuggestions = [];
+
+    if (input.isNotEmpty) {
+      List<ToDo> todos = await _todoService.fetchToDoTasks(isComplete: false);
+      todoSuggestions = todos
+          .where((todo) =>
+              todo.content.toLowerCase().contains(input.toLowerCase()))
+          .toList();
+    }
+    return todoSuggestions;
+  }
+
+  Future<List<FileData>> getDiarySuggestions(String input) async {
+    List<FileData> diarySuggestions = [];
+
+    if (input.isNotEmpty) {
+      List<FileData> files = await _fileService.fetchFiles();
+      diarySuggestions = files
+          .where(
+              (file) => file.title.toLowerCase().contains(input.toLowerCase()))
+          .toList();
+    }
+    return diarySuggestions;
+  }
+}
--- lib/utilities/components/searchbar/box_search_bar.dart
+++ lib/utilities/components/searchbar/box_search_bar.dart
@@ -1,10 +1,30 @@
-/*
-* File Name        : box_search_bar.dart
-* Group            : trOlsz Group
-* Description      : This file contains code for custom search bar.
-*/
-
+import 'package:chronicles/screens/todo/todo_screen.dart';
+import 'package:chronicles/services/search_in_device.dart';
+import 'package:chronicles/utilities/components/date_time/chronicles_date_time.dart';
 import 'package:flutter/material.dart';
+import 'package:chronicles/utilities/components/text_editor/file_data_class.dart';
+import 'package:chronicles/screens/text_editor/chronicles_text_editor.dart';
+
+import '../todo/todo.dart';
+
+TextStyle searchTitles = TextStyle(
+  color: Color(0xFF1F1F1F),
+  fontFamily: 'Hind',
+  fontSize: 18.0,
+  fontWeight: FontWeight.bold,
+);
+TextStyle searchTypes = TextStyle(
+  color: Color(0x801F1F1F),
+  fontFamily: 'Hind',
+  fontSize: 12.0,
+  fontWeight: FontWeight.w400,
+);
+TextStyle searchDateTimes = TextStyle(
+  color: Color(0x801F1F1F),
+  fontFamily: 'Hind',
+  fontSize: 12.0,
+  fontWeight: FontWeight.w400,
+);
 
 class BoxSearchBar extends StatefulWidget {
   const BoxSearchBar({super.key});
@@ -14,11 +34,21 @@ class BoxSearchBar extends StatefulWidget {
 }
 
 class _BoxSearchBarState extends State<BoxSearchBar> {
+  List<ToDo> todoSuggestions = [];
+  List<FileData> diarySuggestions = [];
+  SearchService searchService = SearchService();
+
+  void fetchSuggestion(String input) async {
+    todoSuggestions = await searchService.getTodoSuggestions(input);
+    diarySuggestions = await searchService.getDiarySuggestions(input);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SearchBar(
-      hintText: "Search",
-      hintStyle: WidgetStateProperty.all<TextStyle>(
+    return SearchAnchor.bar(
+      viewBackgroundColor: Color(0xFFFFFFFF),
+      barHintText: "Search",
+      barHintStyle: WidgetStateProperty.all<TextStyle>(
         TextStyle(
           fontSize: 16.0,
           fontFamily: 'Hind',
@@ -26,14 +56,123 @@ class _BoxSearchBarState extends State<BoxSearchBar> {
           color: Color(0xFF1F1F1F),
         ),
       ),
-      leading: Icon(Icons.search, color: Color(0xFF1F1F1F)),
-      backgroundColor: WidgetStateProperty.all<Color>(Colors.white),
-      shadowColor: WidgetStateProperty.all<Color>(Color(0xFF344A53)),
-      elevation: WidgetStateProperty.all<double>(2),
-      padding:
+      barLeading: Icon(Icons.search, color: Color(0xFF1F1F1F)),
+      barBackgroundColor: WidgetStateProperty.all<Color>(Colors.white),
+      barPadding:
           WidgetStateProperty.all<EdgeInsets>(EdgeInsets.fromLTRB(22, 0, 0, 0)),
-      shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(3))),
+      barShape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(3),
+        ),
+      ),
+      onChanged: (input) {
+        setState(() {
+          fetchSuggestion(input.toLowerCase());
+        });
+      },
+      isFullScreen: false,
+      onSubmitted: (query) {
+        if (query.isEmpty) {
+          Navigator.pop(context);
+        }
+        fetchSuggestion(query.toLowerCase());
+      },
+      suggestionsBuilder: (BuildContext context, SearchController controller) {
+        final String input = controller.value.text;
+
+        if (input.isEmpty) {
+          return [];
+        }
+
+        List<Widget> suggestions = [];
+
+        suggestions.addAll(todoSuggestions.map((suggestion) {
+          ChroniclesDateTime dateTime = ChroniclesDateTime(
+              nowTime: DateTime.fromMillisecondsSinceEpoch(
+                  suggestion.millisecondSinceEpoch));
+          return ListTile(
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  suggestion.content,
+                  style: searchTitles,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '::Todo::',
+                      style: searchTypes,
+                    ),
+                    Text(
+                      '${dateTime.getIntDay()} - ${dateTime.getFullStringMonth()} - ${dateTime.getIntYear()}',
+                      style: searchDateTimes,
+                    ),
+                  ],
+                )
+              ],
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return ToDoScreen();
+                  },
+                ),
+              );
+            },
+          );
+        }).toList());
+
+        suggestions.addAll(diarySuggestions.map(
+          (suggestion) {
+            ChroniclesDateTime dateTime = ChroniclesDateTime(
+                nowTime: DateTime.fromMillisecondsSinceEpoch(
+                    suggestion.millisecondSinceEpoch));
+            return ListTile(
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    suggestion.title,
+                    style: searchTitles,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '::Diary::',
+                        style: searchTypes,
+                      ),
+                      Text(
+                        '${dateTime.getIntDay()} - ${dateTime.getFullStringMonth()} - ${dateTime.getIntYear()}',
+                        style: searchDateTimes,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return TextEditor(
+                        isModify: true,
+                        fileName: '${suggestion.millisecondSinceEpoch}.json',
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          },
+        ).toList());
+
+        return suggestions;
+      },
     );
   }
 }
